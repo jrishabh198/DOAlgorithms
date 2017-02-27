a344a5df32d0e37f2034b66e074f4418f574fe50
Rishabh <jrishabh198@gmail.com>
2017-02-28 00:12:40 +0530

    lucky draw added

--- assign2/luckyDraw.cpp
+++ assign2/luckyDraw.cpp
@@ -1,11 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
+int findlis(int ind,int arr[],int n){
+	int t1[20004];
+	int ans =0;
+	for(int i = ind;i<=ind+n;i++){
+		int f=lower_bound(t1+1,t1+ans+1,arr[i])-t1;
+		ans=max(ans,f);
+		t1[f]=arr[i];
+	}
+	return ans;
+}
 int main(){
 	int t;
 	cin>>t;
 	for(int test =0;test<t;test++){
 		int n;
 		cin>>n;
-		
+		int arr[n+n];
+		for(int i =0;i<n;i++){
+			cin>>arr[i];
+			arr[i+n]=arr[i];
+		}
+		int ans =0;
+		for(int i =0;i<n;i++)ans = max(ans,findlis(i,arr,n));
+		cout<<ans<<endl;
 	}
 }
